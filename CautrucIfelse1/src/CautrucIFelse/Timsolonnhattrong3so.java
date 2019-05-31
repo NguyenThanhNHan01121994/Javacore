f29924243b57148b2ac9a4c298a82a3a9a36ba48
nhan <nhannguyen01121994@gmail.com>
2019-05-31 15:23:57 +0700

    Them Lệnh continue cho tímolonnhattrong3so

---
+++ CautrucIfelse1/src/CautrucIFelse/Timsolonnhattrong3so.java
@@ -0,0 +1,43 @@
+package CautrucIFelse;
+import java.util.Scanner;
+class Timsolonnhattrong3so {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a ,b ,c , max ;
+		for (int i = 1 ; i <5 ; i ++ ) {
+			System.out.println(i);
+					
+		java.util.Scanner scanner = new Scanner(System.in);
+		System.out.println("nhap a");
+			a = scanner.nextInt();
+			System.out.println("nhap b");
+				b = scanner.nextInt();
+				System.out.println("nhap c"); 
+				c = scanner.nextInt();
+				if (a<0) {
+					continue;
+				}
+					System.out.println("ChaoBan!");
+				
+			  if ( a >= b) {
+					if( a>=c) {
+						max = a;
+						}
+					else { max = c;
+				}} else if (b >= c) {
+					max = b;
+				} else {max =c ;
+				}
+				System.out.println("so lon nhat trong 3 so" + a + " ,"+ b+" va" + c +"la" + max );
+				}
+				
+				
+				
+
+			}
+		
+
+	}
+
+
