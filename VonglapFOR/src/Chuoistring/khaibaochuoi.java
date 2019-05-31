f0f742153c07eec129020cac1f97623d78a3af69
nhan <nhannguyen01121994@gmail.com>
2019-05-31 16:49:29 +0700

    khaibaochuoi String

---
+++ VonglapFOR/src/Chuoistring/khaibaochuoi.java
@@ -0,0 +1,13 @@
+package Chuoistring;
+
+class khaibaochuoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String chuoi1 = "";
+String chuoi2 ="hello world";
+System.out.println("chuoi rong co gia tri= " + chuoi1);
+System.out.println("chuoi 2 co gia tri="+ chuoi2);
+	}
+
+}
