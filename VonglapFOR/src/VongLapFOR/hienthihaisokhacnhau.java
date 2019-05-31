163bb1376d4b9f52e3fc7ed67ef66f132a9f0a17
nhan <nhannguyen01121994@gmail.com>
2019-05-31 14:49:26 +0700

    Vonglap FOR ,Hienthihaisokhacnhau

---
+++ VonglapFOR/src/VongLapFOR/hienthihaisokhacnhau.java
@@ -0,0 +1,15 @@
+package VongLapFOR;
+import java.util.*;
+class hienthihaisokhacnhau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1, number2;
+		Scanner scanner = new Scanner(System.in);
+		for (number1=0,number2=number1 + 3 ; number2<8 ; number1 ++ , number2 ++) {
+			System.out.println(number1 + "," + number2);
+		}
+
+	}
+
+}
