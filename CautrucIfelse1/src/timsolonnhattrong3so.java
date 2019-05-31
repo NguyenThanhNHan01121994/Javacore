3b89cf29aba8f89946ffdf437739f8fd83b9e16f
nhan <nhannguyen01121994@gmail.com>
2019-05-31 15:16:11 +0700

    Thêm lệnh break

---
+++ CautrucIfelse1/src/timsolonnhattrong3so.java
@@ -0,0 +1,36 @@
+package cautrucifelse;
+import java.util.Scanner;
+class timsolonnhattrong3so {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a ,b ,c , max ;
+		for (int i = 1 ; i <5 ; i ++ ) {
+			System.out.println(i);
+			
+		java.util.Scanner scanner = new Scanner(System.in);
+		System.out.println("nhap a");
+		a = scanner.nextInt();
+		System.out.println("nhap b");
+		b = scanner.nextInt();
+		System.out.println("nhap c");
+		c = scanner.nextInt();
+		if ( a >= b) {
+			if( a>=c) {
+				max = a;
+				}
+			else { max = c;
+		}} else if (b >= c) {
+			max = b;
+		} else {max =c ;
+		}
+		System.out.println("so lon nhat trong 3 so" + a + " ,"+ b+" va" + c +"la" + max );
+		}
+		
+		
+		
+
+	}
+}
+
+
